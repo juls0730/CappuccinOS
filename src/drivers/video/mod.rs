792525189e0b13f73e14168cd9b18ea2bf636ec6
juls0730 <juliangarner240@gmail.com>
2023-08-27 06:29:00 -0500

    stitching memory map & double buffer, although it's slow and needs to be fixed

--- src/drivers/video/mod.rs
+++ src/drivers/video/mod.rs
@@ -6,24 +6,25 @@ pub static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new(0);
 
 // This is slow, but significantly faster than filling the framebuffer pixel-by-pixel with for loops.
 // idk, fix it later ig.
-pub fn fill_screen(color: u32) {
+pub fn fill_screen(color: u32, framebuffer: Option<*mut u8>) {
     if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response().get() {
         if framebuffer_response.framebuffer_count < 1 {
             return;
         }
 
-        let framebuffer = &framebuffer_response.framebuffers()[0];
-        let buffer_size = (framebuffer.pitch * framebuffer.width) as usize;
+        let framebuffer_response = &framebuffer_response.framebuffers()[0];
+        let buffer_size = (framebuffer_response.pitch * framebuffer_response.height) as usize;
+        let mut framebuffer_ptr = framebuffer_response.address.as_ptr().unwrap();
 
-        crate::libs::util::memset32(
-            framebuffer.address.as_ptr().unwrap() as *mut u32,
-            color,
-            buffer_size,
-        );
+        if framebuffer.is_some() {
+            framebuffer_ptr = framebuffer.unwrap()
+        }
+
+        crate::libs::util::memset32(framebuffer_ptr as *mut u32, color, buffer_size);
     }
 }
 
-pub fn put_char(character: char, cx: u16, cy: u16, fg: u32, bg: u32) {
+pub fn put_char(character: char, cx: u16, cy: u16, fg: u32, bg: u32, framebuffer: Option<*mut u8>) {
     let font = font::G_8X16_FONT;
 
     let character_array = font[character as usize];
@@ -36,23 +37,29 @@ pub fn put_char(character: char, cx: u16, cy: u16, fg: u32, bg: u32) {
             let x = (cx * 8 + col) as u32;
             let y = (cy * 16 + row as u16) as u32;
 
-            put_pixel(x, y, if pixel == 1 { fg } else { bg });
+            put_pixel(x, y, if pixel == 1 { fg } else { bg }, framebuffer);
         }
     }
 }
 
-pub fn put_pixel(x: u32, y: u32, color: u32) {
+pub fn put_pixel(x: u32, y: u32, color: u32, framebuffer: Option<*mut u8>) {
     if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response().get() {
         if framebuffer_response.framebuffer_count < 1 {
             return;
         }
 
-        let framebuffer = &framebuffer_response.framebuffers()[0];
+        let framebuffer_response = &framebuffer_response.framebuffers()[0];
+        let mut framebuffer_ptr = framebuffer_response.address.as_ptr().unwrap();
+
+        if framebuffer.is_some() {
+            framebuffer_ptr = framebuffer.unwrap()
+        }
 
         unsafe {
-            let pixel_offset =
-                (y * framebuffer.pitch as u32 + (x * (framebuffer.bpp / 8) as u32)) as isize;
-            *(framebuffer.address.as_ptr().unwrap().offset(pixel_offset) as *mut u32) = color;
+            let pixel_offset = (y * framebuffer_response.pitch as u32
+                + (x * (framebuffer_response.bpp / 8) as u32))
+                as isize;
+            *(framebuffer_ptr.offset(pixel_offset) as *mut u32) = color;
         }
     }
 }
